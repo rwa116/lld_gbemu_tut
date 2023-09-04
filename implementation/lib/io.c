2332b9ef5262e461c81f93276806cd82881f98b2
rwa116 <rwa116@sfu.ca>
2023-09-04 15:59:55 -0700

    Completed Part 12 of LLD gbemu tutorial

--- implementation/lib/io.c
+++ implementation/lib/io.c
@@ -2,10 +2,9 @@
 #include <timer.h>
 #include <dma.h>
 #include <cpu.h>
+#include <lcd.h>
 
 static char serial_data[2];
-
-u8 ly = 0;
 
 u8 io_read(u16 address) {
     if(address == 0xFF01) {
@@ -14,16 +13,14 @@ u8 io_read(u16 address) {
     if(address == 0xFF02) {
         return serial_data[1];
     }
-
     if(BETWEEN(address, 0xFF04, 0xFF07)) {
         return timer_read(address);
     }
     if(address == 0xFF0F) {
         return cpu_get_int_flags();
     }
-
-    if(address == 0xFF44) {
-        return ly++;
+    if(BETWEEN(address, 0xFF40, 0xFF4B)) {
+        return lcd_read(address);
     }
  
     printf("UNSUPPORTED bus read(%04X)\n", address);
@@ -38,7 +35,6 @@ void io_write(u16 address, u8 value) {
         serial_data[1] = value;
         return;
     }
-
     if(BETWEEN(address, 0xFF04, 0xFF07)) {
         timer_write(address, value);
         return;
@@ -47,9 +43,9 @@ void io_write(u16 address, u8 value) {
         cpu_set_int_flags(value);
         return;
     }
-    if(address == 0xFF46) {
-        dma_start(value);
-        printf("DMA START!\n;");
+    if(BETWEEN(address, 0xFF40, 0xFF4B)) {
+        lcd_write(address, value);
+        return;
     }
 
     printf("UNSUPPORTED bus write(%04X)\n", address);
